3655dd677aca786f84c07674310c9330425cd659
Sutg <suen061@126.com>
2019-03-27 11:20:47 +0800

    第二次修改

---
+++ app/src/main/java/com/example/administrator/soundmanager/util/LOG.java
@@ -0,0 +1,22 @@
+package com.example.administrator.soundmanager.util;
+
+import android.util.Log;
+
+public class LOG {
+    private static boolean isDebug=true;
+    public static void d(String tag,String content){
+        if(isDebug){
+            Log.d(tag,content);
+        }
+    }
+    public static void e(String tag,String content){
+        if(isDebug){
+            Log.e(tag,content);
+        }
+    }
+    public static void i(String tag,String content){
+        if(isDebug){
+            Log.i(tag,content);
+        }
+    }
+}
